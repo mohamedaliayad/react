3b614ca081a622f3c886fe36488f9e1320a347b2
mohamedaliayad <140124216+mohamedaliayad@users.noreply.github.com>
2023-09-14 18:17:17 +0300

    Update Front.jsx

--- src/Front.jsx
+++ src/Front.jsx
@@ -5,7 +5,7 @@ export default class Front extends Component {
   render() {
     return (<>
 
-<div  className="home d-flex justify-content-center align-items-center text-white"><div className="text-center"><img _ngcontent-nxp-c17 src="img/img.svg" alt className="mb-3" /><div className="text-center pt-4" style={{color: 'white'}}><h2 className="text-uppercase mb-3 fs-1 fw-bolder">start Framework</h2><div _ngcontent-nxp-c6 className="d-flex align-items-center justify-content-center mb-3"><div _ngcontent-nxp-c6 className="line me-3" style={{backgroundColor: 'white'}} /><i className="fa-solid fa-star" /><div  className="line ms-3" style={{backgroundColor: 'white'}} /></div></div>
+<div  className="home d-flex justify-content-center align-items-center text-white"><div className="text-center"><img  src="img/img.svg" alt className="mb-3" /><div className="text-center pt-4" style={{color: 'white'}}><h2 className="text-uppercase mb-3 fs-1 fw-bolder">start Framework</h2><div className="d-flex align-items-center justify-content-center mb-3"><div className="line me-3" style={{backgroundColor: 'white'}} /><i className="fa-solid fa-star" /><div  className="line ms-3" style={{backgroundColor: 'white'}} /></div></div>
 
 
 <div>Graphic Artist - Web Designer - Illustrator</div></div></div>
